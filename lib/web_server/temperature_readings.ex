f800fb1e868fa35ef1188c79df82e0fb7e27e426
Aaron Abrook <aabrook@gmail.com>
2018-01-20 08:53:33 +1000

    Query events by room

--- lib/web_server/temperature_readings.ex
+++ lib/web_server/temperature_readings.ex
@@ -1,9 +1,20 @@
 defmodule WebServer.TemperatureReadings do
   import Plug.Conn
 
+  def all(conn = %{params: %{ "room" => room } }) do
+    {:ok, events} = :nil
+      |> UUID.uuid5(room)
+      |> EventStore.read_stream_forward()
+
+    response = events |> Poison.encode!()
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, response)
+  end
   def all(conn) do
     conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(200, "Hello world")
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, %{error: "Must specify the query param 'room' to query rooms"} |> Poison.encode!())
   end
 end
