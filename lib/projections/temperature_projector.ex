06a41817352b1397768a270035c107fbace41027
aabrook <aabrook@gmail.com>
2018-05-12 20:23:07 +1000

    Tidy up readings

--- lib/projections/temperature_projector.ex
+++ lib/projections/temperature_projector.ex
@@ -6,18 +6,30 @@ defmodule Projections.TemperatureProjector do
   import Ecto.Query
   alias IotConsumer.EventStoreRepo, as: Repo
 
-  project %TemperatureRecorded{room: room, humidity: humidity, temperature: temperature} = event do
-    case find_room(room) do
-      nil -> changeset = %Projection.Temperature{} |> Projection.Temperature.changeset(%{room: room, humidity: humidity, temperature: temperature})
-        Ecto.Multi.insert(multi, :temperature, changeset)
-      room -> changeset = room |> Projection.Temperature.changeset(%{humidity: humidity, temperature: temperature})
-        Ecto.Multi.update(multi, :temperature, changeset)
-    end
+  project %TemperatureRecorded{room: room, humidity: humidity, temperature: temperature} do
+    room
+    |> find_room()
+    |> save_room(multi, %{room: room, humidity: humidity, temperature: temperature})
   end
 
   defp find_room(room) do
     Projection.Temperature
     |> where(room: ^room)
     |> Repo.one
+  end
+
+  defp save_room(nil, multi, params) do
+    changeset = %Projection.Temperature{}
+      |> Projection.Temperature.changeset(params)
+
+    IO.puts "Saving new room: #{inspect changeset}"
+    Ecto.Multi.insert(multi, :temperature, changeset)
+  end
+  defp save_room(room = %Projection.Temperature{}, multi, params) do
+    changeset = room
+                |> Projection.Temperature.changeset(params)
+
+    IO.puts "Updating room: #{inspect changeset}"
+    Ecto.Multi.update(multi, :temperature, changeset)
   end
 end
